0a52d5febf41dc8eab04e87bdbc3af985999de6c
danielelaterra9 <danielelaterra9@gmail.com>
2026-05-24 16:04:25 +0200

    Add Elite route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
@@ -10,21 +10,35 @@ import Valutazione from "./pages/Valutazione";
 import ValutazioneConferma from "./pages/ValutazioneConferma";
 import Metodo from "./pages/Metodo";
 import Contatti from "./pages/Contatti";
+import Elite from "./pages/Elite";
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const isElite = location.pathname === '/elite';
+  return (
+    <>
+      <Header />
+      {children}
+      {!isElite && <Footer />}
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/servizi" element={<Servizi />} />
-          <Route path="/valutazione" element={<Valutazione />} />
-          <Route path="/valutazione/conferma" element={<ValutazioneConferma />} />
-          <Route path="/metodo" element={<Metodo />} />
-          <Route path="/contatti" element={<Contatti />} />
-        </Routes>
-        <Footer />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/servizi" element={<Servizi />} />
+            <Route path="/valutazione" element={<Valutazione />} />
+            <Route path="/valutazione/conferma" element={<ValutazioneConferma />} />
+            <Route path="/metodo" element={<Metodo />} />
+            <Route path="/elite" element={<Elite />} />
+            <Route path="/contatti" element={<Contatti />} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
       <Toaster 
         position="top-right" 
