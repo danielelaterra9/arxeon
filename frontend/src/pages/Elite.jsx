194f204e1689714ce76c379c8d168e85314d90b2
danielelaterra9 <danielelaterra9@gmail.com>
2026-05-24 16:02:49 +0200

    Add Elite page with iframe

---
+++ frontend/src/pages/Elite.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Elite = () => {
+  return (
+    <div className="min-h-screen bg-[#161716]-">
+      <iframe
+        src="https://autonomous-business-2.preview.emergentagent.com/"
+        title="Arxeon Elite Landing Page"
+        className="wull w-full h-screen border-none"
+        style={{ width: '100%', height: 'calc(100vh - 80px)', margin-top: '80px' }}
+      />
+    </div>
+  );
+};
+
+export default Elite;
